08d3c393bd8aa89a65390a19d445f8a10de5b9ce
xeptao <xeptaowastaken@gmail.com>
2022-05-03 20:07:24 +0300

    feat: template code

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./App.module.scss";
+
+export const App: React.FC = () => {
+  return (
+    <div className={styles.App}>
+      <h1>Hello World 👺</h1>
+    </div>
+  );
+};
